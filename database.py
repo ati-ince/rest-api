e669de4440f605f71b7c1b0479f0b5741682d503
ahmed t ince <atahir.ince@gmail.com>
2022-09-26 23:45:40 +0200

    sync test v2

--- database.py
+++ database.py
@@ -1,22 +1,18 @@
-from click import password_option
 import mysql.connector
-import sys
-import boto3
-import os
 
 ENDPOINT="database-ati.c9axqirfrvzk.eu-central-1.rds.amazonaws.com"
 PORT="3306"
 USER="admin"
 REGION="eu-central-1c"
 DBNAME="the_db"
-
-passwrd = "19881988.MySql"
+PASSWRD = "19881988.MySql"
 
 try:
-    conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=passwrd ,port=PORT, database=DBNAME)
+    conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=PASSWRD ,port=PORT, database=DBNAME)
     cur = conn.cursor()
     cur.execute("""SELECT now()""")
     query_results = cur.fetchall()
     print(query_results)
+    print('hellllooooo !!!!!!!!!!!!!')
 except Exception as e:
     print("Database connection failed due to {}".format(e))     
